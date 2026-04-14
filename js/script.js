cd2f5a943a61d59b5663c860ed53059e40608ba7
Marcus Vinícius Aguiar <marcusviniciusaguiarsoares@gmail.com>
2026-04-14 12:31:41 -0300

    feat: adição de seletores DOM

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// ELEMENTOS
+
+// Formulário de nova tarefa
+const newTaskForm = document.querySelector("#newTaskForm");
+const titleInput = document.querySelector("#titleInput");
+const prioritySelect = document.querySelector("#prioritySelect");
+
+// Busca de tarefas
+const findInput = document.querySelector("#findInput");
+
+// Filtros de exibição
+const priorityFilter = document.querySelector("#priorityFilter");
+const statusSelector = document.querySelector("#statusSelector");
+
+// Tarefas
+const tasksCount = document.querySelector("#tasksCount");
+const taskList = document.querySelector("#taskList");
+
+// Modal de Deleção
+const deleteModal = document.querySelector("#deleteModal");
+const confirmDelete = document.querySelector("#confirmDelete");
+
+// Modal de Edição
+const editModal = document.querySelector("#editModal");
+const editTitleInput = document.querySelector("#editTitleInput");
+const editPriority = document.querySelector("#editPriority");
+const saveEdit = document.querySelector("#saveEdit");
+
