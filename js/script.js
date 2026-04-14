cd5b235c4d8b24cba457a915fced8d2da6be9909
Marcus Vinícius Aguiar <marcusviniciusaguiarsoares@gmail.com>
2026-04-14 13:12:55 -0300

    feat: adição funções reutilizáveis

--- js/script.js
+++ js/script.js
@@ -26,3 +26,56 @@ const editTitleInput = document.querySelector("#editTitleInput");
 const editPriority = document.querySelector("#editPriority");
 const saveEdit = document.querySelector("#saveEdit");
 
+
+
+// ESTADO DA APLICAÇÃO
+let tasks = [];
+let editTaskId;
+let deleteTaskId;
+
+
+// FUNÇÕES REUTILIZÁVEIS
+const gerarId = () => {
+
+    return Date.now();
+
+};
+
+const formatarData = (date) => {
+
+    const d = new Date(date);
+    const day = String(d.getDate()).padStart(2,"0");
+    const month = String(d.getMonth() + 1).padStart(2, "0");
+    const year = String(d.getFullYear());
+    const weekDay = String(d.getDay());
+    const hours = String(d.getHours()).padStart(2, "0");
+    const minutes = String(d.getMinutes()).padStart(2, "0");
+
+    return `${day}/${month}/${year} (${weekDay}) às ${hours}:${minutes}`;
+
+};
+
+const getPriorityText = (priority) => {
+
+    const text = {
+        low: "Baixa prioridade",
+        medium: "Média prioridade",
+        high: "Alta prioridade"
+    };
+    
+    return text[priority];
+
+};
+
+const getPriorityEmoji = (priority) => {
+
+    const emoji = {
+        low: "🟢",
+        medium: "🟡",
+        high: "🔴"
+    };
+
+    return emoji[priority];
+
+};
+
